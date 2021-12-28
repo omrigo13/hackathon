b53a6819afeee46c1f4409bf60e0a8dd7558261b
nono20794 <chardon@post.bgu.ac.il>
2021-12-28 22:33:05 +0200

    change client

--- client.py
+++ client.py
@@ -22,24 +22,25 @@ broadcast_socket.bind(('', udp_port))
 while True:
     try:
         msg, address = broadcast_socket.recvfrom(buffer_size)
-        print("Received offer from " + address[0] + ", attempting to connect...")
-        tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        cookie, msg_type, tcp_server_port = struct.unpack('IBH', msg)
-        if cookie != magic_cookie or msg_type != msg_type:
-            print("wrong broadcast message format")
-            continue
-        tcp_client_socket.connect((address[0], tcp_server_port))
-        tcp_client_socket.send(team_name.encode())
-        msg_received = tcp_client_socket.recv(buffer_size)
-        print(msg_received.decode())
-        reads, _, _ = select([sys.stdin, tcp_client_socket], [], [], timeout)
-        # if len(reads) > 0 and reads[0] == sys.stdin:
-        #     question_answer = sys.stdin.readline()
-        question_answer = getch.getch()
-        tcp_client_socket.send(question_answer.encode())
-        answer = tcp_client_socket.recv(buffer_size)
-        print(answer.decode())
-        tcp_client_socket.close()
+        if address[0] == "172.1.0.40":
+            print("Received offer from " + address[0] + ", attempting to connect...")
+            tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cookie, msg_type, tcp_server_port = struct.unpack('IBH', msg)
+            if cookie != magic_cookie or msg_type != msg_type:
+                print("wrong broadcast message format")
+                continue
+            tcp_client_socket.connect((address[0], tcp_server_port))
+            tcp_client_socket.send(team_name.encode())
+            msg_received = tcp_client_socket.recv(buffer_size)
+            print(msg_received.decode())
+            reads, _, _ = select([sys.stdin, tcp_client_socket], [], [], timeout)
+            # if len(reads) > 0 and reads[0] == sys.stdin:
+            #     question_answer = sys.stdin.readline()
+            question_answer = getch.getch()
+            tcp_client_socket.send(question_answer.encode())
+            answer = tcp_client_socket.recv(buffer_size)
+            print(answer.decode())
+            tcp_client_socket.close()
 
     except Exception as e:
         print(e)
