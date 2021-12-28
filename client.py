bd31cf57e1ccba637af0609a0bac3d0a5ff50ca6
nono20794 <chardon@post.bgu.ac.il>
2021-12-28 16:34:16 +0200

    add listen an connect to sever

--- client.py
+++ client.py
@@ -17,12 +17,19 @@ broadcast_socket.bind(('', udp_port))
 
 while True:
  try:
-    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    msg, address = broadcast_socket.recvfrom(buffer_size)
-    print("Received offer from " + address[0] + ", attempting to connect...")
-    tcp_client_socket.connect((address[0], tcp_server_port))
-    tcp_client_socket.send(team_name.encode())
-    tcp_client_socket.close()
+     tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     msg, address = broadcast_socket.recvfrom(buffer_size)
+     print("Received offer from " + address[0] + ", attempting to connect...")
+     cookie, type, tcp_server_port = struct.unpack('IBH', msg)
+     if (cookie != magic_cookie or type != msg_type):
+         print("wrong broadcast message format")
+     continue
+     tcp_client_socket.connect((address[0], tcp_server_port))
+     tcp_client_socket.send(team_name.encode())
+     msg_received = tcp_client_socket.recv(buffer_size)
+     print(msg_received.decode())
+     math_problem_answer(tcp_client_socket)
+     tcp_client_socket.close()
 
  except:
     tcp_client_socket.close()
