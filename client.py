7a289ac2611fa601788f29ce8bce977f53ff8462
nono20794 <chardon@post.bgu.ac.il>
2021-12-28 17:19:51 +0200

    fix bugs client

--- client.py
+++ client.py
@@ -17,18 +17,17 @@ broadcast_socket.bind(('', udp_port))
 
 while True:
     try:
-        tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         msg, address = broadcast_socket.recvfrom(buffer_size)
         print("Received offer from " + address[0] + ", attempting to connect...")
+        tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         cookie, msg_type, tcp_server_port = struct.unpack('IBH', msg)
         if cookie != magic_cookie or msg_type != msg_type:
             print("wrong broadcast message format")
-        continue
+            continue
         tcp_client_socket.connect((address[0], tcp_server_port))
         tcp_client_socket.send(team_name.encode())
         msg_received = tcp_client_socket.recv(buffer_size)
         print(msg_received.decode())
-        math_problem_answer(tcp_client_socket)
         tcp_client_socket.close()
 
     except:
