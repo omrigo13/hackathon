aef9d497dea8819a5dc89f41628058ca53320165
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-12-28 17:19:24 +0200

    working on main game from server side

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ from random import randint
 from socket import *
 from scapy.arch import get_if_addr, struct
 import time
+from threading import Thread
 
 tcp_server_port = 2040
 magic_cookie = 0xabcddcba
 msg_type = 0x2
 udp_port = 13117
+buffer_size = 1024
 players = {}
 
 def math_problem():
@@ -15,7 +17,8 @@ def math_problem():
     return str(num1) + "+" + str(num2)
 
 def tcp_socket():
-    server_address = get_if_addr('eth1')
+    # server_address = get_if_addr('eth1')
+    server_address = "192.168.0.112"
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((str(server_address), tcp_server_port))
     server_socket.listen()
@@ -27,14 +30,14 @@ def udp_broadcast():
     udp_server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     udp_server_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-    while len(players) < 2:
+    while len(players) < 1:
         msg = struct.pack('IBH', magic_cookie, msg_type, tcp_server_port)
         udp_server_socket.sendto(msg, ('<broadcast>', udp_port))
         time.sleep(1)
 
 def listen_tcp(server_socket):
     global players
-    while len(players) < 2:
+    while len(players) < 1:
         conn, addr = server_socket.accept()
         players[addr] = conn
 
@@ -53,3 +56,18 @@ def end_msg(team1_name, team1_answer, team2_name, team2_answer, answer):
     elif (team1_answer != answer) and team2_answer == answer:
         return correct_answer + winner_msg + team2_name
     return "nobody answered, the game finished with a draw"
+
+def game():
+    global players
+    team1_name = players.get(list(players.keys())[0]).recv(buffer_size).decode()
+    print(team1_name)
+
+server_socket = tcp_socket()
+while True:
+        broadcasts_thread = Thread(target=udp_broadcast)
+        clients_thread = Thread(target=listen_tcp, args=(server_socket, ))
+        broadcasts_thread.start()
+        clients_thread.start()
+        broadcasts_thread.join()
+        clients_thread.join()
+        game()
