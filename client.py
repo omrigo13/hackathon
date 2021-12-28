969de5bba46b2464c637faa7e9e9ffe1c6f6d0ba
nono20794 <chardon@post.bgu.ac.il>
2021-12-28 16:18:56 +0200

    client open socket

--- client.py
+++ client.py
@@ -6,3 +6,11 @@ msg_type = 0x2
 udp_port = 13117
 buffer_size = 1024
 team_name = "blabla"
+
+print("Client started, listening for offer requests...")
+
+#open a socket for broadcast message
+broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+broadcast_socket.bind(('', udp_port))
