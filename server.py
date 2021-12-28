a1760053c7fb24a09edbebcf0bd82cb091c7d58e
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-12-28 16:29:20 +0200

    make udp socket for broadcast with clients

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from random import randint
 from socket import *
-from scapy.arch import get_if_addr
+from scapy.arch import get_if_addr, struct
+import time
 
 tcp_server_port = 2040
+magic_cookie = 0xabcddcba
+msg_type = 0x2
+udp_port = 13117
+players = {}
 
 def math_problem():
     num1 = randint(0,5)
@@ -16,3 +21,13 @@ def tcp_socket():
     server_socket.listen()
     print("Server started, listening on IP address " + str(server_address))
     return server_socket
+
+def udp_broadcast():
+    udp_server_socket = socket(AF_INET, SOCK_DGRAM)
+    udp_server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    udp_server_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+    while len(players) < 2:
+        msg = struct.pack('IBH', magic_cookie, msg_type, tcp_server_port)
+        udp_server_socket.sendto(msg, ('<broadcast>', udp_port))
+        time.sleep(1)
