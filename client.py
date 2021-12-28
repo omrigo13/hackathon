1fa7125336503740192e512eed74be29797517a8
nono20794 <chardon@post.bgu.ac.il>
2021-12-28 23:52:00 +0200

    client update

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import socket
 import struct
-import sys
-import getch
+import msvcrt
+import time
+import keyboard
+from msvcrt import getch
+
 from select import select
 
 magic_cookie = 0xabcddcba
@@ -10,6 +13,8 @@ udp_port = 13117
 buffer_size = 1024
 timeout = 10
 team_name = "blabla"
+
+
 
 print("Client started, listening for offer requests...")
 
@@ -30,19 +35,34 @@ while True:
             continue
         tcp_client_socket.connect((address[0], tcp_server_port))
         tcp_client_socket.send(team_name.encode())
+        print("sent name")
         msg_received = tcp_client_socket.recv(buffer_size)
         print(msg_received.decode())
-        reads, _, _ = select([sys.stdin, tcp_client_socket], [], [], timeout)
+        print("i got here1")
+        # reads, _, _ = select([sys.stdin, tcp_client_socket], [], [], timeout)
+        # print("i got here2")
         # if len(reads) > 0 and reads[0] == sys.stdin:
         #     question_answer = sys.stdin.readline()
-        question_answer = getch.getch()
-        tcp_client_socket.send(question_answer.encode())
-        answer = tcp_client_socket.recv(buffer_size)
-        print(answer.decode())
-        tcp_client_socket.close()
-
+        # question_answer = getch()
+        # print(question_answer)
+        # tcp_client_socket.send(question_answer.encode())
+        # print("sent answer")
+        # answer = tcp_client_socket.recv(buffer_size)
+        # print(answer.decode())
+        # tcp_client_socket.close()
+        #  Windows
+        key= keyboard.read_key()
+        print(key)
+        tcp_client_socket.send(key.encode())
+        print("sent key")
+        try:
+            SummaryMessage = tcp_client_socket.recv(1024)
+            print(SummaryMessage.decode())
+            print("\nServer disconnected, listening for offer requests...")
+        except:
+            print("Server Disconnected")
     except Exception as e:
         print(e)
-        # tcp_client_socket.close()
+        tcp_client_socket.close()
         # continue
         # print("fail")
