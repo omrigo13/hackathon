8664cd7de13d61c656575122c2b3a75d3ab7450c
nono20794 <chardon@post.bgu.ac.il>
2021-12-29 23:07:01 +0200

    fix bugs

--- client.py
+++ client.py
@@ -6,12 +6,12 @@ from threading import Thread
 
 from termcolor import colored
 
-magic_cookie = 0xabcddcba
+magic_cookie = 0xabcddcba;
 msg_type = 0x2
 udp_port = 13117
 buffer_size = 1024
 timeout = 10
-team_name = "ro"
+team_name = "noa with no h"
 gameover =False
 tcp_socket= None
 def __listen_keyboard(): #thread waiting for keyboard
